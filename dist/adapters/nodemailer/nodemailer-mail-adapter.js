81248a366beffa49dc5c72e74346b8c00937f19c
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-05-08 11:19:52 -0300

    ajustes para deploy

---
+++ dist/adapters/nodemailer/nodemailer-mail-adapter.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NodemailerMailAdapter = void 0;
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const transport = nodemailer_1.default.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "91ff3b709f7b25",
+        pass: "f2ed63ba89805a"
+    }
+});
+class NodemailerMailAdapter {
+    async sendMail({ subject, body }) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Tiago de oliveira Gomes <tyagoogomes@gmail.com>',
+            subject,
+            html: body
+        });
+    }
+}
+exports.NodemailerMailAdapter = NodemailerMailAdapter;
